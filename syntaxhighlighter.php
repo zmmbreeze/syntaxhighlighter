0c006ac9da8eb140eea4af82316a053898c74755
MZhou <zmmbreeze0825@gmail.com>
2013-10-28 22:15:28 +0800

    Update syntaxhighlighter.php
    remove js and css hoot

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -63,10 +63,10 @@ class SyntaxHighlighter {
 		add_filter( 'bp_get_the_topic_post_edit_text',    array( $this, 'decode_shortcode_contents' ),                     1 ); // BuddyPress
 
 		// Outputting SyntaxHighlighter's JS and CSS
-		add_action( 'wp_head',                            array( $this, 'output_header_placeholder' ),                     15 );
-		add_action( 'admin_head',                         array( $this, 'output_header_placeholder' ),                     15 ); // For comments
-		add_action( 'wp_footer',                          array( $this, 'maybe_output_scripts' ),                          15 );
-		add_action( 'admin_footer',                       array( $this, 'maybe_output_scripts' ),                          15 ); // For comments
+		// add_action( 'wp_head',                            array( $this, 'output_header_placeholder' ),                     15 );
+		// add_action( 'admin_head',                         array( $this, 'output_header_placeholder' ),                     15 ); // For comments
+		// add_action( 'wp_footer',                          array( $this, 'maybe_output_scripts' ),                          15 );
+		// add_action( 'admin_footer',                       array( $this, 'maybe_output_scripts' ),                          15 ); // For comments
 
 		// Admin hooks
 		add_action( 'admin_init',                         array( $this, 'register_setting' ) );
